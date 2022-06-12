f72f0f1799175c7d541f13422c8875a413e732d8
정형진 <pnescene@icloud.com>
2022-06-12 22:35:39 +0900

    styles the detail screen

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,30 +1,76 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
+import { Dimensions, StyleSheet, useColorScheme } from "react-native";
 import styled from "styled-components/native";
 import Poster from "../components/Poster";
 import { iMedia } from "../utils/api";
+import { makeImagePath } from "../utils/makeImagePath";
+import { LinearGradient } from "expo-linear-gradient";
+import { BLACK_COLOR, WHITE_COLOR } from "../styles/colors";
 
 type RootStackParamList = {
   Detail: iMedia;
 };
 
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
 function Detail({
   navigation: { setOptions },
   route: { params },
 }: NativeStackScreenProps<RootStackParamList, "Detail">) {
+  const isDark = useColorScheme() === "dark";
   useEffect(
-    () => setOptions({ title: "title" in params ? params.title : params.name }),
+    () => setOptions({ title: "title" in params ? "Movie" : "TV Show" }),
     []
   );
   return (
     <Container>
-      <Poster posterPath={params.poster_path || ""} />
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImagePath(params.backdrop_path || "", "w500") }}
+        />
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["transparent", isDark ? BLACK_COLOR : WHITE_COLOR]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Column>
+          <Poster posterPath={params.poster_path || ""} />
+          <Title>
+            {"title" in params ? params.original_title : params.original_name}
+          </Title>
+        </Column>
+      </Header>
+      <Overview>{params.overview}</Overview>
     </Container>
   );
 }
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
+  color: ${(props) => props.theme.textColor};
+`;
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0 24px;
+`;
+const Background = styled.Image``;
+const Column = styled.View`
+  flex-direction: row;
+`;
+const Title = styled.Text`
+  width: 60%;
+  align-self: flex-end;
+  margin-left: 12px;
+  margin-bottom: 12px;
+  color: ${(props) => props.theme.textColor};
+  font-weight: bold;
+  font-size: 24px;
+`;
+const Overview = styled.Text`
+  padding: 24px;
   color: ${(props) => props.theme.textColor};
 `;
 
