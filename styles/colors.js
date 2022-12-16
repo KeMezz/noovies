4be101cc045799e113fd7eab930a06a4b7c379c7
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-16 15:42:35 +0900

    delete everything

--- styles/colors.js
+++
@@ -1,5 +0,0 @@
-export const YELLOW_COLOR = "#f1c40f";
-export const BLACK_COLOR = "#2c3e50";
-export const WHITE_COLOR = "#ecf0f1";
-export const LIGHT_GREY = "#95a5a6";
-export const DARK_GREY = "#7f8c8d";
