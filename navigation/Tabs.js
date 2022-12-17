9f691350d5b9b126a993ae0d1931eafff645ca03
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-17 23:50:34 +0900

    implements icons on tab navigator

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -3,15 +3,43 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movie from "../screens/Movie";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   return (
-    <Tab.Navigator initialRouteName="Movies">
-      <Tab.Screen name="Movies" component={Movie} />
-      <Tab.Screen name="TV" component={Tv} />
-      <Tab.Screen name="Search" component={Search} />
+    <Tab.Navigator
+      initialRouteName="Movies"
+      screenOptions={{ tabBarShowLabel: false }}
+    >
+      <Tab.Screen
+        name="Movies"
+        component={Movie}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="movie" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="TV"
+        component={Tv}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="tv" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="search" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
--- screens/Tv.js
+++ screens/Tv.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 
 const Tv = () => (
   <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <Text>Tv</Text>
+    <Text>TV</Text>
   </View>
 );
 
