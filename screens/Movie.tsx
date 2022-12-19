3443acfeadd67f3a023bccdd9246bc15ff4b6e4b
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-19 16:24:21 +0900

    change to typescript

--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-const Movie = ({ navigation: { navigate } }) => (
+const Movie: React.FC<NativeStackScreenProps<any, "Movies">> = ({
+  navigation: { navigate },
+}) => (
   <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
     <Title>Movie</Title>
   </Btn>
