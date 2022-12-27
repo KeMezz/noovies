7f8a6154f332ade15e4b2bdae8ff4647616252e2
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-27 21:35:57 +0900

    Update Stack.tsx

--- navigation/Stack.tsx
+++ navigation/Stack.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Detail from "../screens/Detail";
-import { NativeStackNavigatorProps } from "@react-navigation/native-stack/lib/typescript/src/types";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const NativeStack = createNativeStackNavigator();
 
