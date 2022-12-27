e4ddb5b9ad6b1399ee8e26af7ecc0255e5046188
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-27 22:10:22 +0900

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -48,6 +48,49 @@ export interface ITv {
   vote_count: number;
 }
 
+export interface IMovieDetail {
+  adult?: boolean;
+  backdrop_path?: string | null;
+  belongs_to_collection?: null | Object;
+  budget?: number;
+  genres?: {
+    id: number;
+    name: string;
+  }[];
+  homepage?: string | null;
+  id?: number;
+  imdb_id?: string | null;
+  original_language?: string;
+  original_title?: string;
+  overview?: string | null;
+  popularity?: number;
+  poster_path?: string | null;
+  production_companies: {
+    id?: number;
+    logo_path?: string | null;
+    name?: string;
+    origin_country?: string;
+  }[];
+  production_countries?: {
+    name?: string;
+    iso_3166_1?: string;
+  }[];
+  release_date?: string;
+  revenue?: number;
+  runtime?: number | null;
+  spoken_languages?: {
+    english_name?: string;
+    iso_639_1?: string;
+    name?: string;
+  }[];
+  status?: string;
+  tagline?: string | null;
+  title?: string;
+  video?: boolean;
+  vote_average?: number;
+  vote_count?: number;
+}
+
 export const fetchMovies = {
   trending: () =>
     fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then((res) =>
